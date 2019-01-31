c0cc46028669bd1179f389effec77f1b97226784
ReynaldoWilly <reynaldowilly@gmail.com>
2019-01-31 12:23:31 -0400

    INICIO DE PROYECTO

---
+++ src/cajachicav2/CajaChicaV2.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package cajachicav2;
+
+import com.cajachica.view.vtnLogin;
+
+/**
+ *
+ * @author reynaldo
+ */
+public class CajaChicaV2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        vtnLogin login =new vtnLogin();
+        login.setVisible(true);
+        
+    }
+    
+}
